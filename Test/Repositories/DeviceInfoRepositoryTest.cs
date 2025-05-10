6edd8bba36e891edd927a7da3e748f9f515404f2
Monkey D Luffy <bredun.denis@gmail.com>
2025-05-10 21:29:54 +0300

    Added [Fact] attribute for the GetDeviceDocRefByCombinedInfo_Shold_Throw_Exception_When_Device_IsNull

--- Test/Repositories/DeviceInfoRepositoryTest.cs
+++ Test/Repositories/DeviceInfoRepositoryTest.cs
@@ -1,11 +1,6 @@
 ﻿using Data_Organizer_Server.Interfaces;
 using Data_Organizer_Server.Repositories;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Test.Repositories
 {
@@ -23,6 +18,7 @@ namespace Test.Repositories
             );
         }
 
+        [Fact]
         public async Task GetDeviceDocRefByCombinedInfo_Shold_Throw_Exception_When_Device_IsNull()
         {
             var repository = new DeviceInfoRepository(collectionFactoryMock.Object);
